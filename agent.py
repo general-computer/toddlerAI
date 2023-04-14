5c7223bf0c9ef370dfb8ae9205bc1060d1820f31
Peter Myers <peter@pcmyers.com>
2023-04-14 18:12:21 -0400

    Initial state inspired by BabyAGI

---
+++ agent.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import time
+from collections import deque
+from typing import Dict, List
+import importlib
+
+import openai
+import pinecone
+from dotenv import load_dotenv
+
+# Load default environment variables from .env file
+load_dotenv()
+
+# Engine configuration
+
+# API Keys
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
+
+OPENAI_API_MODEL = os.getenv("OPENAI_API_MODEL", "gpt-3.5-turbo")
+assert OPENAI_API_MODEL, "OPENAI_API_MODEL environment variable is missing from .env"
+
+if "gpt-4" in OPENAI_API_MODEL.lower():
+    print(
+        "\033[91m\033[1m"
+        + "\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****"
+        + "\033[0m\033[0m"
+    )
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
+assert PINECONE_API_KEY, "PINECONE_API_KEY environment variable is missing from .env"
+
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT", "")
+assert (
+    PINECONE_ENVIRONMENT
+), "PINECONE_ENVIRONMENT environment variable is missing from .env"
+
+# Table config
+YOUR_TABLE_NAME = os.getenv("TABLE_NAME", "")
+assert YOUR_TABLE_NAME, "TABLE_NAME environment variable is missing from .env"
+
+# Goal configuation
+OBJECTIVE = os.getenv("OBJECTIVE", "")
+INITIAL_TASK = os.getenv("INITIAL_TASK", os.getenv("FIRST_TASK", ""))
+
+# Model configuration
+OPENAI_TEMPERATURE = float(os.getenv("OPENAI_TEMPERATURE", 0.0))
+
+# Extensions support begin
+
+def can_import(module_name):
+    try:
+        importlib.import_module(module_name)
+        return True
+    except ImportError:
+        return False
+
+
+DOTENV_EXTENSIONS = os.getenv("DOTENV_EXTENSIONS", "").split(" ")
+
+# Command line arguments extension
+# Can override any of the above environment variables
+ENABLE_COMMAND_LINE_ARGS = (
+    os.getenv("ENABLE_COMMAND_LINE_ARGS", "false").lower() == "true"
+)
+if ENABLE_COMMAND_LINE_ARGS:
+    if can_import("extensions.argparseext"):
+        from extensions.argparseext import parse_arguments
+
+        OBJECTIVE, INITIAL_TASK, OPENAI_API_MODEL, DOTENV_EXTENSIONS = parse_arguments()
+
+# Load additional environment variables for enabled extensions
+if DOTENV_EXTENSIONS:
+    if can_import("extensions.dotenvext"):
+        from extensions.dotenvext import load_dotenv_extensions
+
+        load_dotenv_extensions(DOTENV_EXTENSIONS)
+
+# Check if we know what we are doing
+assert OBJECTIVE, "OBJECTIVE environment variable is missing from .env"
+assert INITIAL_TASK, "INITIAL_TASK environment variable is missing from .env"
+
+if "gpt-4" in OPENAI_API_MODEL.lower():
+    print(
+        "\033[91m\033[1m"
+        + "\n*****USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS*****"
+        + "\033[0m\033[0m"
+    )
+
+# Print OBJECTIVE
+print("\033[94m\033[1m" + "\n*****OBJECTIVE*****\n" + "\033[0m\033[0m")
+print(f"{OBJECTIVE}")
+
+print("\033[93m\033[1m" + "\nInitial task:" + "\033[0m\033[0m" + f" {INITIAL_TASK
+
